4b72a243ed6c1fd9e39414a161191966b8a39f6e
Stepan Grachev <step95@yandex.ru>
2020-02-24 15:28:33 +0300

    Signed-off-by: Stepan Grachev <step95@yandex.ru>

--- Task_3.py
+++
@@ -1,4 +0,0 @@
-n = int(input('Enter number 0 - 9: '))
-nn = n * 11
-nnn = n * 111
-print(n + nn + nnn)
--- Task_6.py
+++
@@ -1,8 +0,0 @@
-result_now = int(input('Please, input your current running distance in km: '))
-result_wanted = int(input('Please, input your desired running distance in km: '))
-days = 1
-while result_now < result_wanted:
-    result_now = result_now * 1.1
-    # print(round(result_now, 2))
-    days = days + 1
-print(f'With our training program you will reach this result only in {days} days!')
